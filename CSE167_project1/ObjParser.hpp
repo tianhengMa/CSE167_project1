842e81f02e9cff83b404b1f4a3e22d8718eddf2c
tianhengMa <tim013@ucsd.edu>
2020-10-05 12:16:23 -0700

    first commit

---
+++ CSE167_project1/ObjParser.hpp
@@ -0,0 +1,13 @@
+//
+//  ObjParser.hpp
+//  CSE167_project1
+//
+//  Created by Matthew Ma on 10/5/20.
+//
+
+#ifndef ObjParser_hpp
+#define ObjParser_hpp
+
+#include <stdio.h>
+
+#endif /* ObjParser_hpp */
